732eef74ea59b14357749c3002bf49eb5d45a62c
Peter Toth <tothpeter08@gmail.com>
2019-09-11 16:05:50 +0200

    Change to posrgres from mysql

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,10 @@
 
 ActiveRecord::Schema.define(version: 2019_09_11_125806) do
 
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "hash_id"
     t.string "time_zone"
